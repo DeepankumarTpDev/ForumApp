01a10487cea003a81ad9a8648bf388ddc222b99c
Deepankumar <deepankumar@testpress.in>
2024-09-30 08:18:23 +0000

    Add Edit functionality for Post using Generic Class-Based Views and Add TestCases for it

--- boards/views.py
+++ boards/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 from .forms import NewTopicForm, PostForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
+from django.views.generic import UpdateView 
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 def home(request):
     boards = Board.objects.all()
@@ -56,3 +59,21 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
